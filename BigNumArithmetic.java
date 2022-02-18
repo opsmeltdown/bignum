4cbf44e94355071425d7c2452e2b39230526e48a
opsmeltdown <x360jon@gmail.com>
2022-02-18 16:17:46 +0000

    initial commit

---
+++ BigNumArithmetic.java
@@ -0,0 +1,19 @@
+//Jon Duncan
+//I pledge
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import java.io.PrintWriter;
+
+public class BigNumArithmetic {
+	public static void main (String args[])throws IOException{
+		try {
+		File input = new File(args[0]);
+		Scanner scan = new Scanner(input);
+
+		}  catch (IOException e){
+		System.out.println("File not found.");
+		}
+	}		
+}
