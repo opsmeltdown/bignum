8f4ee3e512c40a72081b90cd321aa0d1994fe905
opsmeltdown <x360jon@gmail.com>
2022-02-23 15:09:45 +0000

    started on reading string stuff

--- BigNumArithmetic.java
+++ BigNumArithmetic.java
@@ -10,9 +10,21 @@ import java.io.PrintWriter;
 public class BigNumArithmetic {
 	public static void main (String args[])throws IOException{
 		try {
+		//setting up file input, stack, and linked list
 		File input = new File(args[0]);
 		Scanner scan = new Scanner(input);
-
+		Stack stack = new AStack();
+		String line;
+		
+		//loop as long as there is another line
+		while (scan.hasNextLine()) {
+			line = scan.nextLine();
+			//removes extra whitespace from input line
+			line = line.replaceAll("\\s+", " ").trim();
+			System.out.println(line);
+			
+       			}	
+		}	
 		}  catch (IOException e){
 		System.out.println("File not found.");
 		}
