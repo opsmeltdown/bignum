8b1ca3439817df26f0a97d1713391f8d2e48faf2
opsmeltdown <x360jon@gmail.com>
2022-02-24 09:29:46 +0000

    attempt at addition, nullpointerexception error :c

--- BigNumArithmetic.java
+++ BigNumArithmetic.java
@@ -10,22 +10,21 @@ import java.io.PrintWriter;
 public class BigNumArithmetic {
 	public static void main (String args[])throws IOException{
 		try {
-		//setting up file input, stack, and linked list
-		File input = new File(args[0]);
-		Scanner scan = new Scanner(input);
-		Stack stack = new AStack();
-		MathOperations mth = new MathOperations();
-		String line;
-		
-		//loop as long as there is another line
-		while (scan.hasNextLine()) {
-			line = scan.nextLine();
-			//removes extra whitespace from input line
-			line = line.replaceAll("\\s+", " ").trim();
-			System.out.println(line);
-			String[] arrLine = line.split(" ");
-			for (int i = 0; i < arrLine.length; i++) {
-				if (!arrLine[i].equals("+") || !arrLine[i].equals("*") ||!arrLine[i].equals("^")) {
+			//setting up file input, stack, and linked list
+			File input = new File(args[0]);
+			Scanner scan = new Scanner(input);
+			Stack stack = new AStack();
+			MathOperations mth = new MathOperations();
+			String line;
+
+			//loop as long as there is another line
+			while (scan.hasNextLine()) {
+				line = scan.nextLine();
+				//removes extra whitespace from input line
+				line = line.replaceAll("\\s+", " ").trim();
+				String[] arrLine = line.split(" ");
+				for (int i = 0; i < arrLine.length; i++) {
+					/////remove 0's from front of numbers
 					String temp = arrLine[i];
 					for(int j =0; j<temp.length(); j++){
 						if (temp.charAt(j)==0){
@@ -33,17 +32,22 @@ public class BigNumArithmetic {
 						}
 						else break;
 					}
-					arrLine[i]=temp;
-					stack.push(arrLine[i]);
-					
-				} else {
-					mth.calculate(stack.pop(), stack.pop(), arrLine[i]);
+					arrLine[i] = temp;
+
+					if (arrLine[i].equals("=")) break;
+					if (!arrLine[i].equals("+") && !arrLine[i].equals("*") && !arrLine[i].equals("^")) {
+						stack.push(arrLine[i]);
+					} else {
+						//pop 2 numbers and send to calculate method with sign
+						stack.push(mth.calculate((String)stack.pop(), (String)stack.pop(), arrLine[i]));
+						
+					}
 				}
-			}
-       		}	
-			
+			System.out.println(line + " " + stack.pop());
+			}	
+
 		}  catch (IOException e){
-		System.out.println("File not found.");
+			System.out.println("File not found.");
 		}
 	}	
 }	
--- MathOperations.java
+++ MathOperations.java
@@ -1,20 +1,21 @@
 
 public class MathOperations {
-	private int carry;
 	LList<Integer> num1 = new LList<Integer>();
 	LList<Integer> num2 = new LList<Integer>();
 
-	public String calculate (Object a, Object b, String sign) {
+	public String calculate (String a, String b, String sign) {
 		//calls appropriate method depening on sign
 		//and returns what the method returns
-		
-		if (sign.equals('+')) return LLtoString(addition(num1,num2));
-		else if (sign.equals( '*')) return LLtoString(mult(num1,num2));
+		num1 = StringtoLL(a);
+		num2 = StringtoLL(b);
+		if (sign.equals("+")) return LLtoString(addition(num1,num2));
+		else if (sign.equals("*")) return LLtoString(mult(num1,num2));
 		else return LLtoString(exp(num1,num2));
 	}
 
 	private LList<Integer> StringtoLL (String a) {
 		LList<Integer> temp = new LList<Integer>();
+		//iterate thru string adding all numbers to linked list nodes
 		for (int i = 0; i < a.length(); i++) {
 			int num = Character.getNumericValue(a.charAt(i));
 			Integer x = new Integer(num);
@@ -26,20 +27,33 @@ public class MathOperations {
 	private String LLtoString (LList<Integer> a) {
 		//iterate thru LL and concat into string
 		String answer = "";
+		for (int i = 0; i < a.size(); i++) {
+			answer = a.get(i).toString().concat(answer);
+		}
 		return answer;
 	}
 
-	private <Integer> LList<Integer> addition (LList<Integer> a, LList<Integer> b) {
+	private LList<Integer> addition (LList<Integer> a, LList<Integer> b) {
 		//add the 2 LinkedLists inputted and 
 		//return answer list to the calculate method
 		LList<Integer> answer = new LList<Integer>();
-		
-		//TO DO stuff here
-
-		return answer; 
+		int carry = 0;
+		int sum = 0;
+		//somethings wrong here idk its 4:30am goodnight
+/*		for (int i = 0; i < a.size(); i++) {
+			for (int j = 0; j < b.size(); i++) {
+				sum = carry;
+				sum += a.get(i) + b.get(j);
+				carry = sum / 10;
+				sum = sum % 10;
+				answer.add(sum);
+			}
+		}		
+*/
+		return a; 
 	}
 
-	private <Integer> LList<Integer> mult (LList<Integer> a, LList<Integer> b) {
+	private LList<Integer> mult (LList<Integer> a, LList<Integer> b) {
 		//multiply lists and return answer list
 		//will have to use add function
 		LList<Integer> answer = new LList<Integer>();
@@ -49,7 +63,7 @@ public class MathOperations {
 		return answer; 
 	}
 
-	private <Integer> LList<Integer> exp (LList<Integer> a, LList<Integer> b) {
+	private LList<Integer> exp (LList<Integer> a, LList<Integer> b) {
 		//use exponentiation by squaring which i know nothing about right now xD	
 		LList<Integer> answer = new LList<Integer>();
 
