e04071aeda8909ff90751fd2e5ea11df37147924
opsmeltdown <x360jon@gmail.com>
2022-02-27 02:51:31 +0000

    added case for blank lines

--- BigNumArithmetic.java
+++ BigNumArithmetic.java
@@ -20,31 +20,33 @@ public class BigNumArithmetic {
 			//loop as long as there is another line
 			while (scan.hasNextLine()) {
 				line = scan.nextLine();
-				//removes extra whitespace from input line
-				line = line.replaceAll("\\s+", " ").trim();
-				String[] arrLine = line.split(" ");
-				for (int i = 0; i < arrLine.length; i++) {
-					//remove 0's from front of numbers
-					String temp = arrLine[i];
-					while (temp.charAt(0) == '0' && temp.length() > 1) {
-						temp = temp.replaceFirst("0", "");
+				if (line.length() > 0) {
+					//removes extra whitespace from input line
+					line = line.replaceAll("\\s+", " ").trim();
+					String[] arrLine = line.split(" ");
+					for (int i = 0; i < arrLine.length; i++) {
+						//remove 0's from front of numbers
+						String temp = arrLine[i];
+						while (temp.charAt(0) == '0' && temp.length() > 1) {
+							temp = temp.replaceFirst("0", "");
+						}
+						arrLine[i] = temp;
+						if (!arrLine[i].equals("+") && !arrLine[i].equals("*") && !arrLine[i].equals("^")) {
+							stack.push(arrLine[i]);
+						} 
+						//else if (stack.topValue()
+						else {
+							//pop 2 numbers and send to calculate
+							stack.push(mth.calculate((String)stack.pop(), (String)stack.pop(), arrLine[i]));	
+						}
 					}
-					arrLine[i] = temp;
-					if (!arrLine[i].equals("+") && !arrLine[i].equals("*") && !arrLine[i].equals("^")) {
-						stack.push(arrLine[i]);
-					} 
-					//else if (stack.topValue()
-					else {
-					//pop 2 numbers and send to calculate
-					stack.push(mth.calculate((String)stack.pop(), (String)stack.pop(), arrLine[i]));	
+					for (int i = 0; i < arrLine.length; i++) {
+						if (i != 0) System.out.print(" ");
+						System.out.print(arrLine[i]);
 					}
-				}
-			for (int i = 0; i < arrLine.length; i++) {
-				if (i != 0) System.out.print(" ");
-				System.out.print(arrLine[i]);
-			}
 
-			System.out.println(" = " + stack.pop());
+					System.out.println(" = " + stack.pop());
+				}
 			}	
 
 		}  catch (IOException e){
